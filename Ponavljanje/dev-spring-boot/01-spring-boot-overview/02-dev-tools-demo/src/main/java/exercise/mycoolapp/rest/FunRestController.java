7526f054845e66161ef0cae657caa4c7c6e0ae30
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-10-29 09:31:38 +0100

    Exercises

---
+++ Ponavljanje/dev-spring-boot/01-spring-boot-overview/02-dev-tools-demo/src/main/java/exercise/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercise.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Denis
+ */
+@RestController
+public class FunRestController {
+
+    @GetMapping("/")
+    private String sayHello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/workout")
+    private String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    private String getDailyFortune() {
+        return "Today is your lucky day.";
+    }
+
+}
