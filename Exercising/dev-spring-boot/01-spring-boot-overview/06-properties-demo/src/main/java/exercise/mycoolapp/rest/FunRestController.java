c5ac8a3b0c99a9684c21728195dcfd6f4c7441c9
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-10-29 11:32:18 +0100

    Recaping

---
+++ Exercising/dev-spring-boot/01-spring-boot-overview/06-properties-demo/src/main/java/exercise/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercise.mycoolapp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Denis
+ */
+@RestController
+public class FunRestController {
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
+
+    @GetMapping("/")
+    private String sayHello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/workout")
+    private String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    private String getDailyFortune() {
+        return "Today is your lucky day.";
+    }
+
+    @GetMapping("/sunday")
+    private String getSunday() {
+        return "Today is sunday.";
+    }
+
+
+}
