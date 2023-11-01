a6f078dfe605ed3408b5153af8e82a65f3b4e7f5
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-11-01 20:47:22 +0100

    Reorganizing

--- dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student-create/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student-create/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -17,12 +17,27 @@ public class CruddemoApplication {
     @Bean
     public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
         return runner -> {
-            createStudent(studentDAO);
+            // createStudent(studentDAO);
+
+            createMultipleStudents(studentDAO);
         };
     }
 
-    private void createStudent(StudentDAO studentDAO) {
+    private void createMultipleStudents(StudentDAO studentDAO) {
+        // create multiple students
+        System.out.println("Creating 3 student objects ...");
+        Student tempStudent1 = new Student("John", "Doe", "paul@luv2code.com");
+        Student tempStudent2 = new Student("Mary", "Public", "mary@luv2code.com");
+        Student tempStudent3 = new Student("Bonita", "Applebum", "bonita@luv2code.com");
 
+        // save the student objects
+        System.out.println("Saving the students ...");
+        studentDAO.save(tempStudent1);
+        studentDAO.save(tempStudent2);
+        studentDAO.save(tempStudent3);
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
         // create the student object
         System.out.println("Creating new student object ...");
         Student tempStudent = new Student("Paul", "Doe", "paul@luv2code.com");
@@ -33,7 +48,21 @@ public class CruddemoApplication {
 
         // display id of the saved student
         System.out.println("Saved student. Generated id: " + tempStudent.getId());
-
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
